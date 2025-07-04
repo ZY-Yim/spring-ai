8706a834d501fcb8cfc86d3ae0afd4e891218aa5
zhiyuyim <zhiyu_yim@163.com>
2025-07-04 22:53:04 +0800

    增加日志级别配置，通过spring-ai的advisor由aop实现

--- src/main/java/com/yanzhiyu/springai/config/CommonConfiguration.java
+++ src/main/java/com/yanzhiyu/springai/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.yanzhiyu.springai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,8 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心可爱的智能助手，你的名字叫小团团，请以小团团的语气和身份回答问题")
+                // 配置日志Advisor
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 }

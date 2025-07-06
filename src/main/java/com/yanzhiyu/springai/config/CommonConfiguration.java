0325b8717f5a045612bcb4cd3312d302c3a618d0
zhiyuyim <zhiyu_yim@163.com>
2025-07-06 20:26:22 +0800

    引入向量模型

--- src/main/java/com/yanzhiyu/springai/config/CommonConfiguration.java
+++ src/main/java/com/yanzhiyu/springai/config/CommonConfiguration.java
@@ -9,8 +9,15 @@ import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStoreContent;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPooled;
 
 import java.util.List;
 
@@ -31,6 +38,7 @@ public class CommonConfiguration {
         return MessageWindowChatMemory.builder().maxMessages(10).build();
     }
 
+    /*// 还不能用工厂生成，每个client的模式不一样
     @Bean
     public ChatClient chatClient(OllamaChatModel model, ChatClientFactory chatClientFactory) {
         return chatClientFactory.createChatClient(model, SYSTEM_PROMPT);
@@ -39,23 +47,9 @@ public class CommonConfiguration {
     @Bean
     public ChatClient gameChatClient(OpenAiChatModel model, ChatClientFactory chatClientFactory) {
         return chatClientFactory.createChatClient(model, GAME_SYSTEM_PROMPT);
-    }
+    }*/
 
-    // 还不能用工厂生成，每个client的模式不一样
-    @Bean
-    public ChatClient serviceChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
-        return ChatClient.builder(model)
-                .defaultSystem(SERVICE_SYSTEM_PROMPT)
-                // 配置日志Advisor
-                .defaultAdvisors(
-                        new SimpleLoggerAdvisor(),
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
-                )
-                .defaultTools(courseTools)
-                .build();
-    }
 
-    /*
     @Bean
     public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
         return ChatClient
@@ -82,5 +76,45 @@ public class CommonConfiguration {
                 )
                 .build();
     }
-    */
+
+
+    @Bean
+    public ChatClient serviceChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient.builder(model)
+                .defaultSystem(SERVICE_SYSTEM_PROMPT)
+                // 配置日志Advisor
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor(),
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .defaultTools(courseTools)
+                .build();
+    }
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public JedisPooled jedisPooled() {
+        return new JedisPooled(host, port);
+    }
+
+    // 不加点话会有两个EmbeddingModel Bean
+    @Bean
+    public RedisVectorStore vectorStore(OpenAiEmbeddingModel model, JedisPooled jedisPooled) {
+        return RedisVectorStore.builder(jedisPooled, model)
+                .prefix("doc:")
+                .initializeSchema(true)
+                .indexName("spring_ai_redis")
+                .build();
+    }
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(OpenAiEmbeddingModel model) {
+        return SimpleVectorStore.builder(model)
+                .build();
+    }
 }
--- src/test/java/com/yanzhiyu/springai/SpringAiApplicationTests.java
+++ src/test/java/com/yanzhiyu/springai/SpringAiApplicationTests.java
@@ -2,10 +2,21 @@ package com.yanzhiyu.springai;
 
 import com.yanzhiyu.springai.util.VectorDistanceUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,6 +27,9 @@ class SpringAiApplicationTests {
     @Autowired
     private OpenAiEmbeddingModel embeddingModel;
 
+    @Autowired
+    private RedisVectorStore vectorStore;
+    // private SimpleVectorStore vectorStore;
 
     @Test
     void contextLoads() {
@@ -61,6 +75,44 @@ class SpringAiApplicationTests {
         for (float[] textVector : textVectors) {
             System.out.println(VectorDistanceUtils.cosineDistance(queryVector, textVector));
         }
+    }
+
+    @Test
+    public void testVectorStore() throws IOException {
+        Resource resource = new FileSystemResource("中二知识笔记.pdf");
+        // 1.创建PDF的读取器
+        PagePdfDocumentReader reader = new PagePdfDocumentReader(
+                resource, // 文件源
+                PdfDocumentReaderConfig.builder()
+                        // 文本格式化器
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.defaults())
+                        // 每1页PDF作为一个Document
+                        .withPagesPerDocument(1)
+                        .build()
+        );
+        // 2.读取PDF文档，拆分为Document
+        List<Document> documents = reader.read();
+        // 3.写入向量库
+        vectorStore.add(documents);
+        // 4.搜索
+        SearchRequest request = SearchRequest.builder()
+                .query("论语中教育的目的是什么")
+                .topK(10)
+                .similarityThreshold(0.6)
+                // 加了会查不出来,redis内部把Metadata取消掉了，放到一个大json里，SimpleVectorStore可以查出来，还保存的
+                // .filterExpression("file_name == '中二知识笔记.pdf'")
+                .build();
+        List<Document> docs = vectorStore.similaritySearch(request);
+        if (docs == null) {
+            System.out.println("没有搜索到任何内容");
+            return;
+        }
+        for (Document doc : docs) {
+            System.out.println(doc.getId());
+            System.out.println(doc.getScore());
+            System.out.println(doc.getText());
+        }
+
     }
 
 }
